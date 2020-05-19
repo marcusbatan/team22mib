b455fa6c9d2705ebf6fd7b509703095e3ac08b8a
Marcusbatan <markus_batan@hotmail.com>
2020-05-19 10:00:40 +0200

    pushar

--- src/Agent/skapaAliens.java
+++ src/Agent/skapaAliens.java
@@ -5,17 +5,34 @@
  */
 package Agent;
 
+import Validering.Validering;
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.swing.JOptionPane;
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
 /**
  *
  * @author marku
  */
 public class skapaAliens extends javax.swing.JFrame {
 
+    public InfDB idb;
+    private ArrayList<String> omradeLista;
+    private Integer platsAlien;
+
     /**
      * Creates new form skapaAliens
      */
     public skapaAliens() {
         initComponents();
+        this.setLocationRelativeTo(null);
+        try {
+            idb = new InfDB("C:\\db\\MIBDB.FDB");
+        } catch (Exception ettUndantag) {
+
+        }
     }
 
     /**
@@ -27,57 +44,167 @@ public class skapaAliens extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        btnLaggTill = new javax.swing.JButton();
+        cmbTilldelatOmrade = new javax.swing.JComboBox<>();
+        txtRegistreringsDatum = new javax.swing.JTextField();
+        txtNyttLosenAlien = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        txtNamnNyAlien = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        txtTelefonNrAlien = new javax.swing.JTextField();
+        txtAnsvarigAgentAlien = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        btnLaggTill.setText("Lägg till");
+        btnLaggTill.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLaggTillActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Lösen:");
+
+        jLabel2.setText("Regdatum:");
+
+        jLabel3.setText("Namn:");
+
+        jLabel4.setText("Telefon:");
+
+        jLabel5.setText("Ansvarig agent:");
+
+        jLabel6.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel6.setText("Skapa alien");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(194, 194, 194)
+                .addComponent(cmbTilldelatOmrade, javax.swing.GroupLayout.PREFERRED_SIZE, 83, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 135, Short.MAX_VALUE)
+                .addComponent(btnLaggTill)
+                .addGap(26, 26, 26))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel4)
+                        .addGap(144, 144, 144)
+                        .addComponent(txtTelefonNrAlien, javax.swing.GroupLayout.DEFAULT_SIZE, 186, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel5))
+                        .addGap(102, 102, 102)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(txtAnsvarigAgentAlien, javax.swing.GroupLayout.DEFAULT_SIZE, 186, Short.MAX_VALUE)
+                            .addComponent(txtNamnNyAlien)
+                            .addComponent(txtRegistreringsDatum)
+                            .addComponent(txtNyttLosenAlien))))
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 186, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(119, 119, 119))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 53, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtAnsvarigAgentAlien, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel5))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(txtNamnNyAlien, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtNyttLosenAlien, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtRegistreringsDatum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(txtTelefonNrAlien, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(30, 30, 30)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cmbTilldelatOmrade, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnLaggTill))
+                .addGap(26, 26, 26))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
+    private void btnLaggTillActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLaggTillActionPerformed
+        // TODO add your handling code here:
+        Object plats = cmbTilldelatOmrade.getSelectedItem();
+        String tilldelatPlats = String.valueOf(plats);
+
+        if (Validering.textFaltHarVarde(txtAnsvarigAgentAlien) && (Validering.textFaltHarVarde(txtNamnNyAlien) && ((Validering.textFaltHarVarde(txtNyttLosenAlien) && (Validering.textFaltHarVarde(txtRegistreringsDatum) && (Validering.textFaltHarVarde(txtTelefonNrAlien))))))) {
+
+            int AlienId = 0;
+            try {
+                int platsId = Integer.parseInt(idb.fetchSingle("SELECT PLATS_ID FROM PLATS WHERE BENAMNING ='" + tilldelatPlats + "'"));
+                AlienId = Integer.parseInt(idb.fetchSingle("SELECT MAX(ALIEN_ID) FROM ALIEN"));
+                AlienId += 1;
+                idb.insert("INSERT INTO ALIEN "
+                        + "(ALIEN_ID, REGISTRERINGSDATUM, LOSENORD, NAMN, TELEFON, PLATS, ANSVARIG_AGENT) "
+                        + "VALUES (" + AlienId + ", "
+                        + "'" + txtRegistreringsDatum.getText() + "', "
+                        + "'" + txtNyttLosenAlien.getText() + "', "
+                        + "'" + txtNamnNyAlien.getText() + "', "
+                        + "'" + txtTelefonNrAlien.getText() + "', "
+                        + "'" + platsId + "', " + "'" + txtAnsvarigAgentAlien.getText() + "')");
+
+                JOptionPane.showMessageDialog(null, "Alien Ã¤r nu Registrerad!");
+
+            } catch (InfException e) {
+                JOptionPane.showMessageDialog(null, e.getMessage());
+                System.out.println("HÄR ÄR FELET");
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(skapaAliens.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(skapaAliens.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(skapaAliens.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(skapaAliens.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new skapaAliens().setVisible(true);
+    }//GEN-LAST:event_btnLaggTillActionPerformed
+
+    private void gePlatsAlien() {
+
+        try {
+            ArrayList<HashMap<String, String>> omrade = idb.fetchRows("SELECT BENAMNING FROM PLATS");
+            for (HashMap<String, String> plats : omrade) {
+                String omradeLista1 = plats.get("BENAMNING");
+                cmbTilldelatOmrade.addItem(omradeLista1);
             }
-        });
-    }
 
+        } catch (InfException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnLaggTill;
+    private javax.swing.JComboBox<String> cmbTilldelatOmrade;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JTextField txtAnsvarigAgentAlien;
+    private javax.swing.JTextField txtNamnNyAlien;
+    private javax.swing.JTextField txtNyttLosenAlien;
+    private javax.swing.JTextField txtRegistreringsDatum;
+    private javax.swing.JTextField txtTelefonNrAlien;
     // End of variables declaration//GEN-END:variables
 }
